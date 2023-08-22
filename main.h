e2667c3d94f0947038ea5598ddebbfc3784a3144
dev-soufiane <dev.sofyane@gmail.com>
2023-08-22 14:05:39 -0700

    Add main.h file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/*Functions prototypes*/
+
+int print_char(char c);
+int print_string(char *s);
+int _printf(const char *format, ...);
+
+#endif
